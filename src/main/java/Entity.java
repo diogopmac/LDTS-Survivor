482ecd11f0c3376d8ee0ff7ec3d281588a98d9d7
diogo <diogo.campeao@gmail.com>
2024-11-14 15:55:37 +0000

    Entity and Position basic implementation

---
+++ src/main/java/Entity.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public abstract class Entity {
+    protected Position position;
+
+    public Entity(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public Position getPosition() { return position; }
+    public void setPosition(Position position) { this.position = position; }
+    
+}
