9df9fdf8237b3445255eb5e085225e0ec07d5325
Hugo <up202307383@up.pt>
2024-11-15 15:41:01 +0000

    Implemented Arena and added methods for Arena, Application, Game and Survivor to be able to display the Survivor's location

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,10 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
+        try {
+            game.run();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/Survivor.java
+++ src/main/java/Survivor.java
@@ -1,7 +1,18 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
 public class Survivor {
     protected Position position;
 
     public Survivor(Position position) {
         this.position = position;
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.x(), position.y()), "X");
     }
 }
