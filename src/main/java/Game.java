cd2c19657d302718e5251265a22781afd27e966e
diogo <diogo.campeao@gmail.com>
2024-11-18 18:33:31 +0000

    Square font Added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,17 +6,39 @@ import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+//NÃO COLOCAR DEPOIS
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+//NÃO COLOCAR DEPOIS
 
 public class Game {
     private Screen screen;
     public Game() {
         try {
-            int width = 50, height = 20;
-            TerminalSize terminalSize = new TerminalSize(width, height);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal =  terminalFactory.createTerminal();
+            URL resource = getClass().getClassLoader().getResource("square.ttf");
+            assert resource != null;
+            File fontFile = new File(resource.toURI());
+            Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+            Font newfont = font.deriveFont(Font.PLAIN, 4);
+
+            AWTTerminalFontConfiguration cfg = AWTTerminalFontConfiguration.newInstance(newfont);
+
+            int width = 800, height = 220;
+            Terminal terminal = new DefaultTerminalFactory()
+                    .setInitialTerminalSize(new TerminalSize(width, height))
+                    .setTerminalEmulatorFontConfiguration(cfg)
+                    .setForceAWTOverSwing(true)
+                    .createTerminal();
             screen = new TerminalScreen(terminal);
             TextGraphics graphics = screen.newTextGraphics();
 
@@ -27,7 +49,27 @@ public class Game {
             screen.startScreen();
             screen.doResizeIfNecessary();
 
+            screen.startScreen();
+
+            //Só para testar
+            boolean running = true;
+
+            while (running) {
+                KeyStroke keyStroke = screen.readInput();
+                if (keyStroke != null) {
+                    if (keyStroke.getKeyType() == KeyType.Character && keyStroke.getCharacter() == 'q') {
+                        running = false;
+                    }
+                }
+            }
+            screen.stopScreen();
+            //Só para testar
+
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (FontFormatException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
 
