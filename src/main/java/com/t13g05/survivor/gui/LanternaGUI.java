873b538edadb0d3c43a311c59c15fb7419b2550c
Hugo <up202307383@up.pt>
2024-11-19 16:59:43 +0000

    Added action set attribute to LanternaGUI that holds keys pressed

--- src/main/java/com/t13g05/survivor/gui/LanternaGUI.java
+++ src/main/java/com/t13g05/survivor/gui/LanternaGUI.java
@@ -9,16 +9,22 @@ import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 import com.t13g05.survivor.model.Position;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LanternaGUI implements GUI{
     private final Screen screen;
+    private Set<KeyEvent> actionSet = new HashSet<KeyEvent>();
     public LanternaGUI(Screen screen) {
         this.screen = screen;
     }
@@ -26,6 +32,16 @@ public class LanternaGUI implements GUI{
         AWTTerminalFontConfiguration fontConfig = getFontConfiguration();
         Terminal terminal = createTerminal(width, height, fontConfig);
         this.screen = createScreen(terminal);
+
+        ((AWTTerminalFrame)terminal).getComponent(0).addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                actionSet.add(e);
+            }
+            public void keyReleased(KeyEvent e) {
+                actionSet.remove(e);
+            }
+        });
     }
     public AWTTerminalFontConfiguration getFontConfiguration() throws IOException, URISyntaxException, FontFormatException {
         URL resource = getClass().getClassLoader().getResource("fonts/square.ttf");
