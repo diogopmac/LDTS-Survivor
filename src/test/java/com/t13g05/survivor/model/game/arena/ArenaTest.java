6df0ef2becdcf76d1afd2ef435aff179eb2d3065
diogo <diogo.campeao@gmail.com>
2024-11-24 20:10:58 +0000

    Fixed arena testing

--- src/test/java/com/t13g05/survivor/model/game/arena/ArenaTest.java
+++ src/test/java/com/t13g05/survivor/model/game/arena/ArenaTest.java
@@ -2,6 +2,7 @@ package com.t13g05.survivor.model.game.arena;
 
 import com.t13g05.survivor.gui.LanternaGUI;
 import com.t13g05.survivor.model.Position;
+import com.t13g05.survivor.viewer.game.GameViewer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,16 +15,18 @@ public class ArenaTest {
     private int widht = 20;
     private int height = 20;
     private LanternaGUI lanternaGUI;
+    private GameViewer gameViewer;
 
     @BeforeEach
     public void setUp() {
         arena = new Arena(widht,height);
+        gameViewer = new GameViewer(arena);
         lanternaGUI = Mockito.mock(LanternaGUI.class);
     }
 
     @Test
-    public void init_test(){
-        arena.draw(lanternaGUI);
+    public void init_test() throws Exception {
+        gameViewer.draw(lanternaGUI);
         Position position = new Position(widht/2,height/2);
         Mockito.verify(lanternaGUI,times(1)).drawSurvivor(position);
 
@@ -34,8 +37,8 @@ public class ArenaTest {
     }
 
     @Test
-    public void draw_test(){
-        arena.draw(lanternaGUI);
+    public void draw_test() throws Exception {
+        gameViewer.draw(lanternaGUI);
         Mockito.verify(lanternaGUI,times(1)).drawSurvivor(Mockito.any(Position.class));
     }
 }
