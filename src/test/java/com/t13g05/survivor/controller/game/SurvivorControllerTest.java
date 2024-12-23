49895c27539247c5feaddba72d1f3a3949cb9ede
diogo <diogo.campeao@gmail.com>
2024-12-23 04:40:43 +0000

    Survivor controllerTest partially implemented

--- src/test/java/com/t13g05/survivor/controller/game/SurvivorControllerTest.java
+++ src/test/java/com/t13g05/survivor/controller/game/SurvivorControllerTest.java
@@ -1,4 +1,66 @@
 package com.t13g05.survivor.controller.game;
 
+import com.t13g05.survivor.Game;
+import com.t13g05.survivor.gui.Action;
+import com.t13g05.survivor.model.Position;
+import com.t13g05.survivor.model.game.ability.Ability;
+import com.t13g05.survivor.model.game.arena.Arena;
+import com.t13g05.survivor.model.game.element.entity.Survivor;
+import com.t13g05.survivor.model.weapon.Weapon;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.awt.*;
+import java.util.Set;
+
 public class SurvivorControllerTest {
+    private Arena arena;
+    private SurvivorController survivorController;
+    private Survivor survivor;
+    private Game game;
+    private Ability ability;
+
+    @BeforeEach
+    public void setUp() {
+        game = Mockito.mock(Game.class);
+        ability = Mockito.mock(Ability.class);
+
+        arena = new Arena(70, 45);
+        survivor = new Survivor(new Position(1,1), 100, "Bow", ability);
+        arena.setSurvivor(survivor);
+
+        survivorController = new SurvivorController(arena);
+    }
+
+    @Test
+    public void invalidMoveTest(){
+        survivorController.step(game, Set.of(Action.UP), 100);
+        Assertions.assertEquals(new Position(1,1), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(1,0), arena.getSurvivor().getDirection());
+        survivorController.step(game, Set.of(Action.LEFT), 102);
+        Assertions.assertEquals(new Position(1,1), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(1,0), arena.getSurvivor().getDirection());
+    }
+
+    @Test
+    public void validMoveTest(){
+        survivorController.step(game, Set.of(Action.DOWN), 100);
+        Assertions.assertEquals(new Position(1,2), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(0,1), arena.getSurvivor().getDirection());
+
+        survivorController.step(game, Set.of(Action.RIGHT), 150);
+        Assertions.assertEquals(new Position(2,2), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(1,0), arena.getSurvivor().getDirection());
+
+        survivorController.step(game, Set.of(Action.LEFT), 200);
+        Assertions.assertEquals(new Position(1,2), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(-1,0), arena.getSurvivor().getDirection());
+
+
+        survivorController.step(game, Set.of(Action.UP), 250);
+        Assertions.assertEquals(new Position(1,1), arena.getSurvivor().getPosition());
+        Assertions.assertEquals(new Position(0,-1), arena.getSurvivor().getDirection());
+    }
 }
--- src/test/java/com/t13g05/survivor/model/weapon/WeaponTest.java
+++ src/test/java/com/t13g05/survivor/model/weapon/WeaponTest.java
@@ -1,5 +1,7 @@
 package com.t13g05.survivor.model.weapon;
 
+import com.t13g05.survivor.model.Position;
+import com.t13g05.survivor.model.game.element.Projectile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,9 @@ public class WeaponTest {
         Assertions.assertEquals(20, sword.baseDamage);
         Assertions.assertEquals(2, sword.projectileType);
         Assertions.assertEquals(2, sword.range);
+
+        Projectile swordProjectile = sword.createProjectile(new Position(1,1), new Position(1,0));
+        Assertions.assertNotNull(swordProjectile);
     }
 
     @Test
@@ -26,6 +31,9 @@ public class WeaponTest {
         Assertions.assertEquals(10, pistol.baseDamage);
         Assertions.assertEquals(1, pistol.projectileType);
         Assertions.assertEquals(10, pistol.range);
+
+        Projectile pistolProjectile = pistol.createProjectile(new Position(1,1), new Position(1,0));
+        Assertions.assertNotNull(pistolProjectile);
     }
 
     @Test
@@ -38,5 +46,8 @@ public class WeaponTest {
         Assertions.assertEquals(15, bow.baseDamage);
         Assertions.assertEquals(0, bow.projectileType);
         Assertions.assertEquals(18, bow.range);
+
+        Projectile bowProjectile = bow.createProjectile(new Position(1,1), new Position(1,0));
+        Assertions.assertNotNull(bowProjectile);
     }
 }
