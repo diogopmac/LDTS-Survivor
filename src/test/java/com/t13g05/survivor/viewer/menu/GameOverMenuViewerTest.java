fa5440ac879c5fa137eda681a3efa1245848cbdc
diogo <diogo.campeao@gmail.com>
2024-12-24 06:35:07 +0000

    Menu viewer test

--- src/test/java/com/t13g05/survivor/viewer/menu/GameOverMenuViewerTest.java
+++ src/test/java/com/t13g05/survivor/viewer/menu/GameOverMenuViewerTest.java
@@ -1,4 +1,32 @@
 package com.t13g05.survivor.viewer.menu;
 
+import com.t13g05.survivor.Game;
+import com.t13g05.survivor.gui.GUI;
+import com.t13g05.survivor.model.menu.ControlsMenu;
+import com.t13g05.survivor.model.menu.GameOverMenu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class GameOverMenuViewerTest {
+    private GUI gui;
+    private GameOverMenuViewer viewer;
+    private GameOverMenu gameOverMenu;
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        game = Mockito.mock(Game.class);
+        gui = Mockito.mock(GUI.class);
+        gameOverMenu = new GameOverMenu();
+        viewer = new GameOverMenuViewer(gameOverMenu);
+    }
+
+    @Test
+    public void entriesTest()throws Exception{
+        viewer.drawElements(gui);
+        Mockito.verify(gui, Mockito.times(1)).drawText((Game.width-9)/2, Game.height/10+3, "Game Over", "#FFFFFF");
+        Mockito.verify(gui, Mockito.times(1)).drawText((Game.width-5)/2, Game.height/3+3, "Retry", "#FFFF00");
+        Mockito.verify(gui, Mockito.times(1)).drawText((Game.width-17)/2, Game.height/3+8, "Back to Main Menu", "#FFFFFF");
+    }
 }
